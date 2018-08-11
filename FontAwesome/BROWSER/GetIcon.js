8ecc6962055d5fa6da3715599b5d27cd40433824
YJ <hanul@hanul.me>
2018-08-11 22:13:19 +0900

    FontAwesome 버전업

--- FontAwesome/BROWSER/GetIcon.js
+++ FontAwesome/BROWSER/GetIcon.js
@@ -5,7 +5,7 @@ FontAwesome.GetIcon = METHOD((m) => {
 	
 	let fontStyleEl = document.createElement('style');
 	fontStyleEl.type = 'text/css';
-	fontStyleEl.appendChild(document.createTextNode('@import url(/FontAwesome/R/css/font-awesome.min.css);'));
+	fontStyleEl.appendChild(document.createTextNode('@import url(/FontAwesome/R/css/all.min.css);'));
 	document.getElementsByTagName('head')[0].appendChild(fontStyleEl);
 	
 	return {
